00d2ddd0d77392c0a6adf86bff61d7e8f8d5bb5d
Johan Giroux <johan.giroux@gmail.com>
2017-11-09 10:31:34 -0800

    Remove FSCalendar dependency

--- voluxe-customer/voluxe-customer/Controller/Main/SchedulePickup/DateTimePickupViewController.swift
+++ voluxe-customer/voluxe-customer/Controller/Main/SchedulePickup/DateTimePickupViewController.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 import UIKit
-import FSCalendar
 
-class DateTimePickupViewController: VLPresentrViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+class DateTimePickupViewController: VLPresentrViewController {
     
     fileprivate let gregorian = Calendar(identifier: .gregorian)
     fileprivate let formatter: DateFormatter = {
@@ -19,37 +18,13 @@ class DateTimePickupViewController: VLPresentrViewController, FSCalendarDataSour
         return formatter
     }()
     
-    fileprivate weak var calendar: FSCalendar!
+    let calendar = UICollectionView()
+    
    
     private func initCalendar() {
-        let calendar = FSCalendar(frame: .zero)
-        calendar.dataSource = self
-        calendar.delegate = self
-        calendar.allowsMultipleSelection = true
-        containerView.addSubview(calendar)
         
-        calendar.calendarHeaderView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.1)
-        calendar.calendarWeekdayView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.1)
-        calendar.appearance.eventSelectionColor = UIColor.white
-        calendar.appearance.eventOffset = CGPoint(x: 0, y: -7)
-        calendar.today = nil // Hide the today circle
-        calendar.register(VLCalendarCell.self, forCellReuseIdentifier: "cell")
-        //        calendar.clipsToBounds = true // Remove top/bottom line
         
-        calendar.pagingEnabled = false
-        calendar.scrollEnabled = false
-        calendar.allowsMultipleSelection = false
-        calendar.allowsSelection = true
-        
-        calendar.calendarWeekdayView.isHidden = true
-        calendar.weekdayHeight = 0
-        //calendar.preferredWeekdayHeight = 0
-
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.empty(_:)))
-        calendar.handleScopeGesture(panGesture)
-        calendar.swipeToChooseGesture.isEnabled = false // Swipe-To-Choose
-        
-        self.calendar = calendar
+        self.view.addSubview(calendar)
     }
     
     override func setupViews() {
@@ -74,112 +49,5 @@ class DateTimePickupViewController: VLPresentrViewController, FSCalendarDataSour
         return (350) + VLPresentrViewController.baseHeight + 60
     }
     
-    @objc func empty(_ sender:UIPanGestureRecognizer){
-        print("empty")
-    }
-    
-    // MARK:- FSCalendarDataSource
-    
-    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
-        let cell = calendar.dequeueReusableCell(withIdentifier: "cell", for: date, at: position)
-        return cell
-    }
-    
-    func calendar(_ calendar: FSCalendar, willDisplay cell: FSCalendarCell, for date: Date, at position: FSCalendarMonthPosition) {
-        self.configure(cell: cell, for: date, at: position)
-    }
-    
-    func calendar(_ calendar: FSCalendar, titleFor date: Date) -> String? {
-        return nil
-    }
-    
-    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-        return 2
-    }
-    
-    // MARK:- FSCalendarDelegate
-    
-    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
-        self.calendar.frame.size.height = bounds.height
-    }
-    
-    func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition)   -> Bool {
-        return monthPosition == .current
-    }
-    
-    func calendar(_ calendar: FSCalendar, shouldDeselect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
-        return monthPosition == .current
-    }
-    
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print("did select date \(self.formatter.string(from: date))")
-        self.configureVisibleCells()
-    }
-    
-    func calendar(_ calendar: FSCalendar, didDeselect date: Date) {
-        print("did deselect date \(self.formatter.string(from: date))")
-        self.configureVisibleCells()
-    }
-    
-    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventDefaultColorsFor date: Date) -> [UIColor]? {
-        if self.gregorian.isDateInToday(date) {
-            return [UIColor.orange]
-        }
-        return [appearance.eventDefaultColor]
-    }
-    
-    // MARK: - Private functions
-    
-    private func configureVisibleCells() {
-        calendar.visibleCells().forEach { (cell) in
-            let date = calendar.date(for: cell)
-            let position = calendar.monthPosition(for: cell)
-            self.configure(cell: cell, for: date!, at: position)
-        }
-    }
-    
-    private func configure(cell: FSCalendarCell, for date: Date, at position: FSCalendarMonthPosition) {
-        
-        let diyCell = (cell as! VLCalendarCell)
-        // Custom today circle
-        diyCell.circleImageView.isHidden = !self.gregorian.isDateInToday(date)
-        // Configure selection layer
-        if position == .current {
-            
-            var selectionType = SelectionType.none
-            
-            if calendar.selectedDates.contains(date) {
-                let previousDate = self.gregorian.date(byAdding: .day, value: -1, to: date)!
-                let nextDate = self.gregorian.date(byAdding: .day, value: 1, to: date)!
-                if calendar.selectedDates.contains(date) {
-                    if calendar.selectedDates.contains(previousDate) && calendar.selectedDates.contains(nextDate) {
-                        selectionType = .middle
-                    }
-                    else if calendar.selectedDates.contains(previousDate) && calendar.selectedDates.contains(date) {
-                        selectionType = .rightBorder
-                    }
-                    else if calendar.selectedDates.contains(nextDate) {
-                        selectionType = .leftBorder
-                    }
-                    else {
-                        selectionType = .single
-                    }
-                }
-            }
-            else {
-                selectionType = .none
-            }
-            if selectionType == .none {
-                diyCell.selectionLayer.isHidden = true
-                return
-            }
-            diyCell.selectionLayer.isHidden = false
-            diyCell.selectionType = selectionType
-            
-        } else {
-            diyCell.circleImageView.isHidden = true
-            diyCell.selectionLayer.isHidden = true
-        }
-    }
 }
 
