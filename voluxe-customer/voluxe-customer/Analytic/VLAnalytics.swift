c451028261ab5a1a4af7b479d028f45445a1ddaf
Johan Giroux <johan.giroux@gmail.com>
2017-12-13 16:56:13 -0800

    Add state change event

--- voluxe-customer/voluxe-customer/Analytic/VLAnalytics.swift
+++ voluxe-customer/voluxe-customer/Analytic/VLAnalytics.swift
@@ -11,8 +11,19 @@ import FirebaseAnalytics
 
 class VLAnalytics {
     
+    static let needServiceScreen = "needServiceScreen"
+    static let serviceTypeParam = "serviceType"
+
+    
+    static let stateChangeEvent = "stateChange"
+    static let stateParam = "state"
+    
     static func logEventWithName(_ eventName: String) {
         Analytics.logEvent(eventName, parameters: nil)
+    }
+    
+    static func logEventWithName(_ eventName: String, paramName: String, paramValue: String) {
+        Analytics.logEvent(eventName, parameters: [paramName : paramValue])
     }
     
     static func logEventWithName(_ eventName: String, parameters: [String:String]) {
