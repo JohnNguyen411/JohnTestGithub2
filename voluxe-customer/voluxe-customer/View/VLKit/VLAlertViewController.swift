9d07aebb2e106e7d077fab6e64e5230ec1f91bc6
Johan Giroux <johan.giroux@gmail.com>
2018-04-27 14:04:56 -0700

    Present persistent Custom AlertVC

---
+++ voluxe-customer/voluxe-customer/View/VLKit/VLAlertViewController.swift
@@ -0,0 +1,9 @@
+//
+//  VLAlertViewController.swift
+//  voluxe-customer
+//
+//  Created by Giroux, Johan on 4/27/18.
+//  Copyright © 2018 Luxe - Volvo Cars. All rights reserved.
+//
+
+import Foundation
