6904a367a5785c7d78b2ed8e660d3f5ebed5369a
Christoph <info@hermiteer.com>
2018-06-13 15:53:40 -0700

    Fixed tests to use the same localized placeholder text as the Customer app, this should avoid future breakage. (#238)

--- shared/tests/String+Test.swift
+++ shared/tests/String+Test.swift
@@ -27,3 +27,19 @@ extension String {
         return "TestPassword\(Int.random(from: 1000, to: 9999))"
     }
 }
+
+/// This (admittedly) is a little sneaky.  The Customer project defines
+/// an NSLocalizedString extension to skip the comment argument.  The
+/// UI test target needs that same extension to use localized strings,
+/// necessary for looking up fields by placeholder name, but the Bundle
+/// that NSLocalizedString uses is not the same as the Customer app
+/// Bundle.  So this extension makes the target conform to the localizations
+/// declared in the Customer target.
+///
+/// Downside to this method is that if the extension signature changes
+/// in the Customer app, the tests will break and this signature will
+/// need to be updated.
+func NSLocalizedString(_ key: String) -> String {
+    let bundle = Bundle(for: BotUserData.self)
+    return NSLocalizedString(key, bundle: bundle, comment: "")
+}
