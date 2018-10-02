e4f46da49b54789dcedfaac98b2dd10343ed27ea
Johan Giroux <johan.giroux@gmail.com>
2018-10-02 16:28:47 -0700

    fix calendar crash

--- voluxe-customer/voluxe-customer/Controller/Main/Scheduling/Modals/DateTimeViewController.swift
+++ voluxe-customer/voluxe-customer/Controller/Main/Scheduling/Modals/DateTimeViewController.swift
@@ -314,7 +314,7 @@ class DateTimeViewController: VLPresentrViewController, FSCalendarDataSource, FS
     
     override func onButtonClick() {
         if let delegate = delegate {
-            if let currentSlots = currentSlots {
+            if let currentSlots = currentSlots, getButtonSelectedIndex() > -1 && currentSlots.count > getButtonSelectedIndex() {
                 let timeSlot = currentSlots[getButtonSelectedIndex()]
                 delegate.onDateTimeSelected(timeSlot: timeSlot)
             } else {
@@ -451,14 +451,16 @@ class DateTimeViewController: VLPresentrViewController, FSCalendarDataSource, FS
                 selectedDate = nextDay
             }
             
-            if selectedDate != nil && selectedDate! > self.maxDate {
+            if selectedDate != nil && (selectedDate! > self.maxDate && selectedDate! >= self.minDate) {
                 selectedDate = nil
             }
             
             if let selectedDate = selectedDate {
-                _ = self.calendar(self.calendar, shouldSelect: selectedDate, at: .current)
-                self.calendar.select(selectedDate)
-                self.showError(error: false)
+                let canSelect = self.calendar(self.calendar, shouldSelect: selectedDate, at: .current)
+                if canSelect {
+                    self.calendar.select(selectedDate)
+                    self.showError(error: false)
+                }
             } else {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.25, execute: {
                     self.calendar.scroll(to: self.maxDate, animated: false)
@@ -741,7 +743,7 @@ class DateTimeViewController: VLPresentrViewController, FSCalendarDataSource, FS
         self.calendar.frame.size.height = bounds.height
     }
     
-    func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition)   -> Bool {
+    func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
         let selectable = dateIsSelectable(date: date)
         if selectable {
             updateSlots(slots: getSlotsForDate(date: date, withLoaner: loanerRequired))
