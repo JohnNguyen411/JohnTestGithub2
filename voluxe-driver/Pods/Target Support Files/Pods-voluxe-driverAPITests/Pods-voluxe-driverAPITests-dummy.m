2cf4c9b28f6fa49749e2f5135ced7b8f005d3a24
Christoph <info@hermiteer.com>
2018-10-24 16:24:09 -0700

    Delete the voluxe-driver folder because the entire directory has been rebuilt and git is too stupid to effectively rebase it. (#421)

--- voluxe-driver/Pods/Target Support Files/Pods-voluxe-driverAPITests/Pods-voluxe-driverAPITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_voluxe_driverAPITests : NSObject
-@end
-@implementation PodsDummy_Pods_voluxe_driverAPITests
-@end
--- voluxe-driver/Pods/Target Support Files/Pods-voluxe-driverUnitTests/Pods-voluxe-driverUnitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_voluxe_driverUnitTests : NSObject
-@end
-@implementation PodsDummy_Pods_voluxe_driverUnitTests
-@end
