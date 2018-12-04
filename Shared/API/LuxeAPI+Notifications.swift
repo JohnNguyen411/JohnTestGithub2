c656f0ba17117eec3b2f156d1d474414f60152ed
Johan Giroux <johan.giroux@gmail.com>
2018-12-04 14:17:23 -0800

    Replace  by

--- Shared/API/LuxeAPI+Notifications.swift
+++ Shared/API/LuxeAPI+Notifications.swift
@@ -3,7 +3,7 @@
 //  voluxe-driver
 //
 //  Created by Christoph on 11/2/18.
-//  Copyright © 2018 Luxe By Volvo. All rights reserved.
+//  Copyright © 2018 Volvo Valet. All rights reserved.
 //
 
 import Foundation
